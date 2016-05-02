5fa9ff53d451238a1ffadda534c65f8aa510a58c
Simon Wilkins <simonwilkins1970@googlemail.com>
2016-05-02 19:18:27 +0100

    Update terraform config

--- configuration-service.tf
+++ configuration-service.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    access_key = "AKIAJCLENHYHE5ICHMAQ"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "eu-west-1"
 }
 
 resource "aws_instance" "config-service" {
     ami = "ami-3f34bd4c"
     instance_type = "t2.micro"
-    key_name ="sw-key-pair-euwest1"
+    key_name =""
     
     provisioner "remote-exec" {
         inline = [
@@ -16,7 +16,7 @@ resource "aws_instance" "config-service" {
         ]
         connection {
           user = "ec2-user"
-          private_key = "${file("C:\\Users\\TEMP\\.ssh\\sw-key-pair-euwest1.pem")}"
+          private_key = "${file("C:\\Users\\?????\\.ssh\\??????.pem")}"
         }
     }
 }
